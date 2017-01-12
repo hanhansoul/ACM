7fefdf3971b00fdceb92b21d4657b7e25ba8bda5
hanhansoul <whr2290@163.com>
2017-01-12 16:58:42 +0800

    482C 20170112

--- codeforces/482C.cpp
+++ codeforces/482C.cpp
@@ -21,9 +21,20 @@
 #include <climits>
 
 using namespace std;
+#define MAXN (1 << 20)
 
+int N;
+string S[55];
+double F[MAXN][25];     // R[msk][i]记录msk下的第i组的期望值
+int R[MAXN][25];        // R[msk][i]记录msk下的第i组包含的字符串，为一个bit mask。
+int C[MAXN][25];        // R[msk][i]记录msk下的第i组包含的字符串的数量
 int main()
 {
 	freopen("input.txt", "r", stdin);
+	scanf("%d", &N);
+	for(int i=1;i<=N;i++)
+        cin >> S[i];
+    len = S[1].length();
+
 	return 0;
 }
