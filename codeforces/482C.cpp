ff8e31adf26b23779b0ba6e3375e58245c91f207
hanhansoul <whr2290@163.com>
2017-01-20 16:57:19 +0800

    482C.cpp

--- codeforces/482C.cpp
+++ codeforces/482C.cpp
@@ -22,6 +22,7 @@
 
 using namespace std;
 #define MAXN (1 << 20)
+#define LL long long
 
 int N;
 string S[55];
@@ -29,23 +30,30 @@ string S[55];
 //int R[MAXN][55];        // R[msk][i]记录msk下的第i组包含的字符串，为一个bit mask。
 //int C[MAXN][55];        // C[msk][i]记录msk下的第i组包含的字符串的数量。
 
-vector<char> E;
-map<int, double> F[MAXN];
-map<int, double>::iterator it;
+// set<char> E[55];
+
+map<int, LL> F[MAXN];
+map<int, LL>::iterator it;
+map<char, int> E[55];
+map<char, int>::const_iterator eit;
+LL R[55];
+int len;
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	scanf("%d", &N);
 	for (int i = 0; i < N; i++)
+	{
 		cin >> S[i];
-	len = S[0].length();
-	memset(D, 0, sizeof(D));
-	for (char ch = 'a'; ch <= 'z'; ch++) E.push_back(ch);
-	for (char ch = 'A'; ch <= 'Z'; ch++) E.push_back(ch);
-	F[0][1] = 0, R[0][1] = (1 << (N - 1)), C[0][1] = N;
+		len = S[i].length();
+		for (int j = 0; j < len; j++)
+			E[j][S[i][j]] |= (1 << i);
+	}
 
-	for (int msk = 0; msk < (1 << (N - 1)); msk++)
+	int full = (1 << N) - 1;
+	F[full][0] = 1;
+	for (int msk = full; msk >= 0; msk--)
 		for (it = F[msk].begin(); it != F[msk].end(); it++)
 			for (int p = 0; p < len; p++)
 			{
@@ -53,20 +61,23 @@ int main()
 				int key = it->first;
 				double val = it->second;
 				if (key & v) continue;
-				int nn = msk, mm = 0;
-				for (int k = 0; k < E.size(); k++)
-				{
-				    int cnt = 0;
-					for (int i = 0; i < N; i++)
-						if (!(msk & (1 << i)) && S[i][p] == E[k])
-						{
-							nn |= (1 << i);
-							cnt++;
-						}
-                    // (key | v)次数
-                    if(cnt) F[nn][key | v] = val ;
-				}
+
+				for (eit = E[p].begin(); eit != E[p].end(); eit++)
+					F[(msk & E[p]->second)][key | v] += val;
 			}
+
+	LL tot = 0LL;
+	for (int i = 0; i < N; i++)
+	{
+		int k = (1 << i);
+		for (it = F[k].begin(); it != F[k].end(); it++)
+		{
+			int c = __builtin_popcount(it->first);
+			R[c] += F[k]->second;
+			tot += F[k]->second;
+		}
+	}
+
 
 	return 0;
 }
